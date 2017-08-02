5963f7ccfdbcf4f88d937f9ef71a98e4cce99183
Vladimir Kunin <v.b.kunin@gmail.com>
2017-08-02 20:30:13 +0300

    Абстрактный класс и интерфейс

---
+++ en.dict.knowitop-template-base.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2017 Vladimir Kunin <v.b.kunin@gmail.com>
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+
+    'Class:ObjectTemplate' => 'Object Template',
+    'Class:ObjectTemplate+' => 'Object Template',
+    'Class:ObjectTemplate/Name' => '%1$s',
+    'Class:ObjectTemplate/Attribute:name' => 'Name',
+    'Class:ObjectTemplate/Attribute:name+' => 'Name of template',
+    'Class:ObjectTemplate/Attribute:description' => 'Description',
+    'Class:ObjectTemplate/Attribute:description+' => 'Description of template',
+    'Class:ObjectTemplate/Attribute:status' => 'Status',
+    'Class:ObjectTemplate/Attribute:status/Value:inactive' => 'inactive',
+    'Class:ObjectTemplate/Attribute:status/Value:active' => 'active',
+
+    'ObjectTemplate:baseinfo' => 'General Information',
+    'ObjectTemplate:template' => 'Template',
+));
---
+++ ru.dict.knowitop-template-base.php
@@ -0,0 +1,23 @@
+<?php
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+
+    'Class:ObjectTemplate' => 'Шаблон объекта',
+    'Class:ObjectTemplate+' => 'Шаблон объекта',
+    'Class:ObjectTemplate/Name' => '%1$s',
+    'Class:ObjectTemplate/Attribute:name' => 'Название',
+    'Class:ObjectTemplate/Attribute:name+' => 'Название шаблона',
+    'Class:ObjectTemplate/Attribute:description' => 'Описание',
+    'Class:ObjectTemplate/Attribute:description+' => 'Описание шаблона',
+    'Class:ObjectTemplate/Attribute:status' => 'Статус',
+    'Class:ObjectTemplate/Attribute:status+' => 'Статус',
+    'Class:ObjectTemplate/Attribute:status/Value:inactive' => 'Неактивный',
+    'Class:ObjectTemplate/Attribute:status/Value:active' => 'Активный',
+    'Class:ObjectTemplate/Attribute:finalclass' => 'Тип',
+
+    'ObjectTemplate:baseinfo' => 'Основная информация',
+    'ObjectTemplate:template' => 'Шаблон',
+    'Menu:ObjectTemplate' => 'Шаблоны объектов',
+    'Menu:ObjectTemplate+' => 'Шаблоны объектов'
+
+));
